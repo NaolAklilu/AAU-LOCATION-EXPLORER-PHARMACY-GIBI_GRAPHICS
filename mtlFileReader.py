f6fcc8546b0b09029eaa8ccaaae8c2bf04a436bb
naolaklilu <naolaklilu12@gmail.com>
2022-06-19 09:57:36 +0300

    add object mtl file reader method

---
+++ mtlFileReader.py
@@ -0,0 +1,30 @@
+import pygame
+from OpenGL.GL import *
+
+def MTL(filename='house0.mtl'):
+    contents = {}
+    mtl = None
+    for line in open(filename, "r"):
+        if line.startswith('#'): continue
+        values = line.split()
+        if not values: continue
+        if values[0] == 'newmtl':
+            mtl = contents[values[1]] = {}
+        elif mtl is None:
+            raise ValueError("mtl file doesn't start with newmtl stmt")
+        elif values[0] == 'map_Kd':
+            mtl[values[0]] = values[1]
+            surf = pygame.image.load(mtl['map_Kd'])
+            image = pygame.image.tostring(surf, 'RGBA', True)
+            ix, iy = surf.get_rect().size
+            texid = mtl['texture_Kd'] = glGenTextures(1)
+            glBindTexture(GL_TEXTURE_2D, texid)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                GL_LINEAR)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+                GL_LINEAR)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ix, iy, 0, GL_RGBA,
+                GL_UNSIGNED_BYTE, image)
+        else:
+            mtl[values[0]] = list(map(float, values[1:]))
+    return contents
